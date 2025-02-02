39d1dc35d659469a5fb08537615f029f076c1fdc
Abdulahad Hussain <129290085+ABDULAHAD118@users.noreply.github.com>
2025-02-03 01:13:08 +0500

    Update weatherImages.ts

--- constants/weatherImages.ts
+++ constants/weatherImages.ts
@@ -1,4 +1,4 @@
-type WeatherCondition = 
+type WeatherCondition =
   | "Partly Cloudy"
   | "Partly Cloudy "
   | "Moderate rain"
@@ -31,41 +31,44 @@ type WeatherCondition =
   | "Moderate or heavy rain with thunder "
   | "Mist"
   | "Mist "
+  | "Fog"
+  | "Fog "
   | "other";
 
 export const weatherImages: Record<WeatherCondition, any> = {
-  "Partly Cloudy": require('../assets/images/partlycloudy.png'),
-  "Partly Cloudy ": require('../assets/images/partlycloudy.png'),
-  "Moderate rain": require('../assets/images/moderaterain.png'),
-  "Moderate rain ": require('../assets/images/moderaterain.png'),
-  "Patchy rain nearby": require('../assets/images/moderaterain.png'),
-  "Patchy rain nearby ": require('../assets/images/moderaterain.png'),
-  "Sunny": require('../assets/images/sun.png'),
-  "Sunny ": require('../assets/images/sun.png'),
-  "Clear": require('../assets/images/sun.png'),
-  "Clear ": require('../assets/images/sun.png'),
-  "Overcast": require('../assets/images/cloud.png'),
-  "Overcast ": require('../assets/images/cloud.png'),
-  "Cloudy": require('../assets/images/cloud.png'),
-  "Cloudy ": require('../assets/images/cloud.png'),
-  "Light rain": require('../assets/images/moderaterain.png'),
-  "Light rain ": require('../assets/images/moderaterain.png'),
-  "Light rain shower": require('../assets/images/moderaterain.png'),
-  "Light rain shower ": require('../assets/images/moderaterain.png'),
-  "Moderate rain at times": require('../assets/images/moderaterain.png'),
-  "Moderate rain at times ": require('../assets/images/moderaterain.png'),
-  "Heavy rain": require('../assets/images/heavyrain.png'),
-  "Heavy rain ": require('../assets/images/heavyrain.png'),
-  "Heavy rain at times": require('../assets/images/heavyrain.png'),
-  "Heavy rain at times ": require('../assets/images/heavyrain.png'),
-  "Moderate or heavy freezing rain": require('../assets/images/heavyrain.png'),
-  "Moderate or heavy freezing rain ": require('../assets/images/heavyrain.png'),
-  "Moderate or heavy rain shower": require('../assets/images/heavyrain.png'),
-  "Moderate or heavy rain shower ": require('../assets/images/heavyrain.png'),
-  "Moderate or heavy rain with thunder": require('../assets/images/heavyrain.png'),
-  "Moderate or heavy rain with thunder ": require('../assets/images/heavyrain.png'),
-  "other": require('../assets/images/moderaterain.png'),
-  "Mist": require('../assets/images/mist.png'),
-  "Mist ": require('../assets/images/mist.png'),
+  "Partly Cloudy": require("../assets/images/partlycloudy.png"),
+  "Partly Cloudy ": require("../assets/images/partlycloudy.png"),
+  "Moderate rain": require("../assets/images/moderaterain.png"),
+  "Moderate rain ": require("../assets/images/moderaterain.png"),
+  "Patchy rain nearby": require("../assets/images/moderaterain.png"),
+  "Patchy rain nearby ": require("../assets/images/moderaterain.png"),
+  "Sunny": require("../assets/images/sun.png"),
+  "Sunny ": require("../assets/images/sun.png"),
+  "Clear": require("../assets/images/sun.png"),
+  "Clear ": require("../assets/images/sun.png"),
+  "Overcast": require("../assets/images/cloud.png"),
+  "Overcast ": require("../assets/images/cloud.png"),
+  "Cloudy": require("../assets/images/cloud.png"),
+  "Cloudy ": require("../assets/images/cloud.png"),
+  "Light rain": require("../assets/images/moderaterain.png"),
+  "Light rain ": require("../assets/images/moderaterain.png"),
+  "Light rain shower": require("../assets/images/moderaterain.png"),
+  "Light rain shower ": require("../assets/images/moderaterain.png"),
+  "Moderate rain at times": require("../assets/images/moderaterain.png"),
+  "Moderate rain at times ": require("../assets/images/moderaterain.png"),
+  "Heavy rain": require("../assets/images/heavyrain.png"),
+  "Heavy rain ": require("../assets/images/heavyrain.png"),
+  "Heavy rain at times": require("../assets/images/heavyrain.png"),
+  "Heavy rain at times ": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy freezing rain": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy freezing rain ": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy rain shower": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy rain shower ": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy rain with thunder": require("../assets/images/heavyrain.png"),
+  "Moderate or heavy rain with thunder ": require("../assets/images/heavyrain.png"),
+  "other": require("../assets/images/moderaterain.png"),
+  "Mist": require("../assets/images/mist.png"),
+  "Mist ": require("../assets/images/mist.png"),
+  "Fog": require("../assets/images/mist.png"),
+  "Fog ": require("../assets/images/mist.png"),
 };
-
